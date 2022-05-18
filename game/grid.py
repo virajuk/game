18018c5471144fb4e4ca46d75dd27aa8cac8857c
virajuk <virajudayakumara@gmail.com>
2022-05-18 07:16:48 -0400

    adding trees & scouts

--- game/grid.py
+++ game/grid.py
@@ -8,32 +8,71 @@ logger = get_logger('my_app')
 class GGrid:
 
     def __init__(self, size: int) -> None:
-        self._width = size
-        self._height = size
-        self.grid = np.ones([self._height, self._width, 3])
+        self.width = size
+        self.height = size
+        self.grid = np.ones([self.height, self.width, 3])
+        self.trees = []
+        self.scouts = []
 
-    def save_png(self) -> None:
-
-        self.grid[0, 2, :] = (255, 0, 0)
-        self.grid[0, 3, :] = (0, 255, 0)
+    def save_grid(self) -> None:
 
         logger.info(f"{self.__class__.__name__} Image : {self.grid.shape}")
-
         cv2.imwrite('images/grid.png', self.grid)
 
     def examine_grid(self) -> None:
-
-        image = cv2.imread('/home/viraj/HUSTLE/GAME/images/gimp.png')
-        # cv2.imshow("image", image)
-        # cv2.waitKey()
-
-        logger.info(f"{self.__class__.__name__} Image : {image.shape}")
-        logger.info(f"{self.__class__.__name__} Image : {image[:, :, 0]}")
-        logger.info(f"{self.__class__.__name__} Image : {image[:, :, 1]}")
-        logger.info(f"{self.__class__.__name__} Image : {image[:, :, 2]}")
+        logger.info(f"{self.__class__.__name__} Trees : {len(self.trees)}")
+        logger.info(f"{self.__class__.__name__} Trees : {len(self.scouts)}")
 
 
 class GTree:
 
     def __init__(self):
-        pass
+        self.color = (0, 255, 0)
+        self.coordinates = ()
+
+    def plant(self, x: int, y: int, grid: GGrid) -> None:
+
+        assert (x <= (grid.width - 1) and y <= (grid.height - 1)), "Can't plant a tree out side grid"
+
+        assert not self.already_planted(grid), "This tree already planted"
+
+        self.coordinates = (x, y)
+
+        grid.grid[x, y, :] = self.color
+        grid.save_grid()
+
+        grid.trees.append(self)
+
+        logger.info(f"{self.__class__.__name__} Coordinates : {self.coordinates}")
+
+    def already_planted(self, grid: GGrid) -> bool:
+
+        if self in grid.trees:
+            return True
+        return False
+
+
+class GScout:
+
+    def __init__(self):
+        self.color = (255, 0, 0)
+        self.coordinates = ()
+        self.movements = ["N", "W", "S", "E"]
+
+    def place(self, x: int, y: int, grid: GGrid) -> None:
+
+        assert (x <= (grid.width - 1) and y <= (grid.height - 1)), "Can't go out side grid"
+
+        self.coordinates = (x, y)
+        grid.grid[x, y, :] = self.color
+        grid.save_grid()
+
+        grid.scouts.append(self)
+
+        logger.info(f"{self.__class__.__name__} Coordinates : {self.coordinates}")
+
+    def move(self, direction: str):
+
+        assert direction in self.movements, "Can't move"
+
+
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from game import GGrid
+from game import GGrid, GTree, GScout
 
-grid = GGrid(5)
-grid.save_png()
+grid = GGrid(9)
+# grid.save_png()
 # grid.examine_grid()
+
+tree1 = GTree()
+tree1.plant(0, 2, grid)
+
+tree2 = GTree()
+tree2.plant(4, 0, grid)
+
+scout = GScout()
+scout.place(4, 1, grid)
+
+scout.move("N")
+
+grid.examine_grid()
